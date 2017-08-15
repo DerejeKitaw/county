23f3a4c0979a832762d3a42f0d322e8c2aa9e22c
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-08-15 00:07:40 -0400

    edit component in progress TODO

--- src/app/countys/county-edit/county-edit.component.ts
+++ src/app/countys/county-edit/county-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, AfterViewInit, OnDestroy, ViewChildren, ElementRef} 
 import { ICounty } from '../county';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountyService } from '../county.service';
-import { FormBuilder, FormGroup, FormControl, FormArray, Validators, FormControlName } from "@angular/forms";
+import { FormBuilder, FormGroup, FormControl, FormArray, Validators, FormControlName, AbstractControl } from "@angular/forms";
 import { Subscription } from "rxjs/Subscription";
 
 @Component({
@@ -11,91 +11,114 @@ import { Subscription } from "rxjs/Subscription";
   styleUrls: ['./county-edit.component.sass']
 })
 export class CountyEditComponent implements OnInit, OnDestroy {
-    @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
     pageTitle: string = 'County Edit';
-    errorMessage: string;
-    CountyId: string;
     countyForm: FormGroup;
-    county: ICounty;
+    formError: { [id: string]: string };
+    errorMessage: string;
+    //CountyId: string;
+    county: ICounty = new ICounty();
     private sub: Subscription;
-    private currentCounty: ICounty;
-    private originalCounty: ICounty;
+    private currentCounty;
+    private originalCounty;
     private dataIsValid: { [key: string]: boolean } = {};
     // Use with the generic validation message class
     displayMessage: { [key: string]: string } = {};
     private validationMessages: { [key: string]: { [key: string]: string } };
 
 
-    get isDirty(): boolean {
-        return JSON.stringify(this.originalCounty) !== JSON.stringify(this.currentCounty);
-    }
+    // get isDirty(): boolean {
+    //     return JSON.stringify(this.originalCounty) !== JSON.stringify(this.currentCounty);
+    // }
     constructor(private fb: FormBuilder,
                 private route: ActivatedRoute,
                 private router: Router,
                 private countyService: CountyService,
                 ) { 
-                    this.route.params.subscribe(params => {
-                        this.CountyId = params.id;
-                        console.log('CountyId :'+this.CountyId);
-                        // Defines all of the validation messages for the form.
-                        // These could instead be retrieved from a file or database.
-                        this.validationMessages = {
-                                countyName: {
-                                    required: 'County name is required.',
-                                    minlength: 'County name must be at least three characters.',
-                                    maxlength: 'County name cannot exceed 50 characters.'
-                                }
-                        };
+                    this.createForm();
+                    // this.logNameChange();
+                    // Defines all of the validation messages for the form.
+                    // These could instead be retrieved from a file or database.
+                    this.validationMessages = {
+                        countyName: {
+                            required: 'County name is required.',
+                            minlength: 'County name must be at least three characters.',
+                            maxlength: 'County name cannot exceed 50 characters.'
+                    }
+                };
+                
+                this.countyForm.valueChanges
+                .subscribe(data => this.onValueChanged(data));
 
-
-                    });
+                    
 
         }
-
+        createForm() {
+            this.countyForm = this.fb.group({
+                countyName: [this.county.countyName, [Validators.required,
+                                   Validators.minLength(3),
+                                   Validators.maxLength(50)]]
+    
+            });
+          }
     ngOnInit(): void {
-        this.countyForm = this.fb.group({
-            countyName: ['', [Validators.required,
-                               Validators.minLength(3),
-                               Validators.maxLength(50)]],
-            countyCode: ['', Validators.required],
-            tags: this.fb.array([]),
-            description: ''
+        this.route.params.subscribe(params => {
+            const id = +params['id'];
+            console.log('CountyId from ngOnInit:'+id);
+            this.getCounty(id);
         });
 
-        // Read the county Id from the route parameter
-        this.sub = this.route.params.subscribe(
-            params => {
-                let id = +params['id'];
-                this.getCounty(id);
-            }
-        );
+        
+        
     }
+
+    save(): void {
+        console.log('Saved: ' + JSON.stringify(this.countyForm.value));
+    }
+
 
     getCounty(id: number): void {
         this.countyService.getCounty(id)
             .subscribe(
-                (county: ICounty) => this.onCountyRetrieved(county),
+                county => this.onCountyRetrieved(county),
                 (error: any) => this.errorMessage = <any>error
             );
     }
-    onCountyRetrieved(county: ICounty): void {
+    onCountyRetrieved(county): void {
         if (this.countyForm) {
             this.countyForm.reset();
+            console.log('this.countyForm is true:');
         }
         this.county = county;
-
+        console.log('this.countyForm is false:'+this.county.countyName);
         if (this.county.id === 0) {
             this.pageTitle = 'Add County';
         } else {
             this.pageTitle = `Edit County: ${this.county.countyName}`;
         }
-
-        // Update the data on the form
-        this.countyForm.patchValue({
-            countyName: this.county.countyName,
-            
-        });
         
+        // // Update the data on the form
+        // this.countyForm.patchValue({
+        //     countyName: this.county.countyName,
+            
+        // });
+        
+    }
+     // Start of a generic validator
+     onValueChanged(data: any): void {
+        for (const field in this.formError) {
+            if (this.formError.hasOwnProperty(field)) {
+                const hasError = this.countyForm.controls[field].dirty &&
+                    !this.countyForm.controls[field].valid;
+                this.formError[field] = '';
+                if (hasError) {
+                    for (const key in this.countyForm.controls[field].errors) {
+                        if (this.countyForm.controls[field].errors.hasOwnProperty(key)) {
+                            this.formError[field] += this.validationMessages[field][key] + ' ';
+                        }
+                    }
+                }
+            }
+        }
     }
     deleteCounty(): void {
 
--- src/app/countys/county.ts
+++ src/app/countys/county.ts
@@ -1,20 +1,38 @@
 /* Defines the county entity */
-export interface ICounty {
-// _id: number;   
-id: number;
-WindSpeed: number; 
-groundSnowLoad: number; 
-FireLaneRequirement: string;
-RoofMountIncludesWellAndSeptic: boolean;
-RoofMountIncludesSitePlan: boolean;
-FootingPlanRequired: boolean;
-RapidShutdown: boolean;
-Stampeddrawing: string;
-Certletter: number;
-NEC: string;
-IBC: string;
-IRC: string; 
-countyName: string;
-age: number;
-isActive: boolean;
+export class ICounty {
+    // comment
+        //id: number;
+        // WindSpeed: number; 
+        // groundSnowLoad: number; 
+        // FireLaneRequirement: string;
+        // RoofMountIncludesWellAndSeptic: boolean;
+        // RoofMountIncludesSitePlan: boolean;
+        // FootingPlanRequired: boolean;
+        // RapidShutdown: boolean;
+        // Stampeddrawing: string;
+        // Certletter: number;
+        // NEC: string;
+        // IBC: string;
+        // IRC: string; 
+        // countyName: string;
+        // age: number;
+        // isActive: boolean;
+    constructor(
+        public id: number =0,
+        public WindSpeed: number =null, 
+        public groundSnowLoad: number =null, 
+        public FireLaneRequirement: string ='',
+        public RoofMountIncludesWellAndSeptic: boolean =true,
+        public RoofMountIncludesSitePlan: boolean =true,
+        public FootingPlanRequired: boolean =true,
+        public RapidShutdown: boolean =true,
+        public Stampeddrawing: string ='',
+        public Certletter: number =null,
+        public NEC: string ='',
+        public IBC: string ='',
+        public IRC: string ='', 
+        public countyName: string ='',
+        public age: number =null,
+        public isActive: boolean =true,
+    ){}
 }
